2b4437860cff78aa75613a9f44d31671d09abdd1
Mário Lucas <mariolucasdev@gmail.com>
2024-02-20 11:37:56 -0300

    compoents structure and reuse

---
+++ src/components/HomePage/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="main">
+            <NavbarTemplate />
+            <h1>{{ message }}</h1>
+            <p>
+            This is a simple component vue.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavbarTemplate from './../Template/Navbar.vue'
+
+export default {
+    name: 'HomePage',
+    props: {
+        message: String
+    },
+    components: {
+        NavbarTemplate
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 40px;
+    margin: 40px 0 0;
+}
+p {
+    font-size: 24px;
+    color: #42b983;
+}
+</style>
